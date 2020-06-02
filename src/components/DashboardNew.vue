73af5f97d42bfaa8a5ee81fad042ca78d02df1ab
yingyang <393050600@qq.com>
2020-06-02 10:57:54 +0800

    图片轮播图456456

--- src/components/DashboardNew.vue
+++ src/components/DashboardNew.vue
@@ -2,35 +2,31 @@
     <div>
 
         <div class="container" align="center">
-            <!--<el-row :gutter="20">-->
-
-                <!--<el-col :xs="24" :sm="24" :lg="24">-->
-                    <!--<el-upload-->
-                            <!--action="https://jsonplaceholder.typicode.com/posts/"-->
-                            <!--list-type="picture-card"-->
-                            <!--:on-remove="handleRemove">-->
-                        <!--<i class="el-icon-plus"></i>-->
-                    <!--</el-upload>-->
-                    <!--<el-dialog :visible.sync="dialogVisible">-->
-                        <!--<img width="100%" :src="dialogImageUrl" alt="">-->
-                    <!--</el-dialog>-->
-                <!--</el-col>-->
-            <!--</el-row>-->
 
             <el-row :gutter="20">
 
                 <el-col :xs="24" :sm="24" :lg="24">
-                        <el-carousel
-                                :autoplay="false"
-                                :interval="3000"
-                                type="card"
-                                height="550px"
-                                style="margin-top: 7%">
-                            <el-carousel-item v-for="item in imageList" :key="item">
-                                <el-image :src=item></el-image>
-                                <el-tag type="warning">标签四</el-tag>
-                            </el-carousel-item>
-                        </el-carousel>
+                    <el-upload
+                            list-type="picture-card"
+                            :disabled="true"
+                            :file-list="fileList">
+                    </el-upload>
+
+
+                </el-col>
+            </el-row>
+            <el-row :gutter="20">
+                <el-col :xs="24" :sm="24" :lg="24" style="text-align: center">
+                    <el-carousel
+                            :autoplay="true"
+                            :interval="1000"
+                            type="card"
+                            height="530px"
+                            style="margin-top: 1%">
+                        <el-carousel-item v-for="item in imageList" :key="item">
+                            <el-image :src=item></el-image>
+                        </el-carousel-item>
+                    </el-carousel>
                 </el-col>
             </el-row>
         </div>
@@ -45,6 +41,8 @@
 
         data() {
             return {
+                fileList: [],
+
                 imageList: [
                     'https://vue-image.oss-cn-shenzhen.aliyuncs.com/DSCF0395.JPG',
                     'https://vue-image.oss-cn-shenzhen.aliyuncs.com/DSCF0411.JPG',
@@ -60,13 +58,19 @@
                     'https://vue-image.oss-cn-shenzhen.aliyuncs.com/DSCF0307.JPG',
                     'https://vue-image.oss-cn-shenzhen.aliyuncs.com/DSCF0337.JPG',
                     'https://vue-image.oss-cn-shenzhen.aliyuncs.com/DSCF0392.JPG',
-                    'https://vue-image.oss-cn-shenzhen.aliyuncs.com/DSCF0393.JPG',
+                    'https://vue-image.oss-cn-shenzhen.aliyuncs.com/DSCF0393.JPG'
                 ],
                 dialogImageUrl: 'https://vue-image.oss-cn-shenzhen.aliyuncs.com/DSCF0395.JPG',
                 dialogVisible: false
             }
         },
         mounted() {
+            this.imageList.forEach((item, index, arr) => {
+                let file={};
+                file.url=item;
+                file.name='asdasd'+index;
+                this.fileList.push(file);
+            })
 
 
             window.addEventListener('resize', function () {
