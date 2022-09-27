414d9bf6f40f07551618d001033e81615591d80b
leon <2944@winnermedical.com>
2022-09-27 19:59:42 +0800

    网站备案号

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <router-view>
+
+
+    </router-view>
+    <app-footer >
+      <div style="position:fixed;text-align:center;bottom:0;margin:0 auto;width:100%;color: #5c6b77">
+        <a href="https://beian.miit.gov.cn/" target="_blank">粤ICP备2021162406号-1</a>
+      </div>
+    </app-footer>
+
   </div>
 </template>
+
+
 <style>
   html,
   body,
